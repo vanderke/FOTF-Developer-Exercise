bb53c6af0dbf82d33060bb6fced67b4b6ff845c7
Dave Gillie <davecgillie@gmail.com>
2016-01-01 14:23:14 -0600

    test case added for assigning variables to the view

--- ready-wed-site/app/controllers/home_controller.rb
+++ ready-wed-site/app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def index
     @chapters = Chapter.all
   end
+
+  def meet
+  end
 end
--- ready-wed-site/config/routes.rb
+++ ready-wed-site/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/meet', to: 'home#meet'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

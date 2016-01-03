aef940f3bb94a1ab2cd5a932f9316e3741d07bc7
Dave Gillie <davecgillie@gmail.com>
2016-01-03 14:26:43 -0600

    update the user model a little

--- ready-wed-site/app/models/user.rb
+++ ready-wed-site/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\w+\.[a-z]+\z/i
+	before_save { email.downcase! }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	#Match start of string, one or more word character plus hyphen or dot, literal at sign, one or more letter digit hyphen or dot, one or more word character, literal dot, one or more letter, end of string
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true,

da2687710768a4ca0a5ac260504bfd04f4373084
Dave Gillie <davecgillie@gmail.com>
2016-01-03 23:15:27 -0600

    Clean up a little

--- ready-wed-site/app/controllers/home_controller.rb
+++ ready-wed-site/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@user = User.find(session[:user_id]) unless !User.exists?(:id => session[:user_id])
+  	@name = User.find(session[:user_id]).name unless !User.exists?(:id => session[:user_id])
     @chapters = Chapter.all
   end
 

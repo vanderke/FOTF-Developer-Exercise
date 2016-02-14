8d8b449662d02237cd6b8063aefae4d92506fc2b
ryanchristian259 <ryan@ryanchristianmedia.com>
2016-02-13 22:52:45 -0700

    a

--- server/app.js
+++ server/app.js
@@ -22,8 +22,8 @@ var app = express();
 
 var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/rtw-lessons';
 
-var port = process.env.PORT || 5000;
-// app.listen('port', (process.env.PORT || 5000));
+// var port = process.env.PORT || 5000;
+app.listen('port', (process.env.PORT || 5000));
 
 mongoose.connect(mongoURI, function(err, res) {
   if(err) {

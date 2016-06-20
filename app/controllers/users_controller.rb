296ed79787257018db68060c8d6b41029d9fe066
timproctor <timandcamiproctor@gmail.com>
2016-06-20 12:45:07 -0600

    refactore sign-in and sign-up, failing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Thanks, #{@user.username} for signing up!"
     else
       render :new

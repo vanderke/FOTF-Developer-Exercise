a37fd42cb60c7e5413c731e2b45b20b36f4b43ec
timproctor <timandcamiproctor@gmail.com>
2016-06-19 18:25:23 -0600

    failing signing-in specs

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -13,4 +13,36 @@ describe "Signing in" do
     expect(page).to have_field("Password")
   end
 
+  it "signs in the user if the username/password combination is valid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+
+    click_button 'Sign In'
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it "does not sign in the user if the username/password combination is invalid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "no match"
+
+    click_button 'Sign In'
+
+    expect(page).to have_text('Invalid')
+  end
+
 end

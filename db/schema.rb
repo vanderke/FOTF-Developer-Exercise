c88cf138061085e3265b77cd0a05146b531d2c36
timproctor <timandcamiproctor@gmail.com>
2016-06-20 21:11:45 -0600

    added a default false to admin user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620223233) do
+ActiveRecord::Schema.define(version: 20160621030847) do
 
   create_table "lessons", force: true do |t|
     t.string   "chapter"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20160620223233) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
   end
 
 end

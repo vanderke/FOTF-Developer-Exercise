043222b7ab6fc807f6e1e99747f15f7c0f7278cf
timproctor <timandcamiproctor@gmail.com>
2016-06-24 07:50:41 -0600

    Getting error messages back.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        session[:user_id] = @user.id
-        format.html { redirect_to @user, notice: "Thanks, #{@user.username} for signing up!" }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity}
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user, notice: "Thanks, #{@user.username} for signing up!"
+    else
+      render :new 
     end
   end
 
